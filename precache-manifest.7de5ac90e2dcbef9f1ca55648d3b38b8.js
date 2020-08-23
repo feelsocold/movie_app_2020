6d9a11a954926551f50dbc193cd397d24300c90c
feelsocold <feelsocoold@gmail.com>
2020-08-23 19:01:07 +0900

    Updates

---
+++ precache-manifest.7de5ac90e2dcbef9f1ca55648d3b38b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aec389f819027486770a5e2f7bcbbdd",
+    "url": "/movie_app_2020.git/index.html"
+  },
+  {
+    "revision": "d1fca14eb3231aa6e9e1",
+    "url": "/movie_app_2020.git/static/css/main.30d362e6.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020.git/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020.git/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1fca14eb3231aa6e9e1",
+    "url": "/movie_app_2020.git/static/js/main.42aeeffe.chunk.js"
+  },
+  {
+    "revision": "1b9eb25e299a9a2b954d",
+    "url": "/movie_app_2020.git/static/js/runtime-main.952d2416.js"
+  }
+]);
